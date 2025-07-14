655a9283736a7f533b8c2d97b993a92abdec9f8b
Bryan Serrato <bryanjesus8@hotmail.com>
2025-07-14 01:01:44 -0500

    Delete src directory

--- src/GestorTramitesSingleton.java
+++
@@ -1,9 +0,0 @@
-
-
-public class GestorTramitesSingleton {
-    private static GestorTramites instancia = new GestorTramites();
-
-    public static GestorTramites getInstancia() {
-        return instancia;
-    }
-}
